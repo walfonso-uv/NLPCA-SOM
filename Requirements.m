bf8cc420befc8256f43afe3a61065d90259062cd
Wilfredo Alfonso <60269522+walfonso-uv@users.noreply.github.com>
2020-01-25 15:37:17 -0500

    Add files via upload

---
+++ Requirements.m
@@ -0,0 +1,14 @@
+%% Requirements
+% Requirements.m checks if you have the toolbox to use this code. The
+% scripts and functions work is you are using MatLab version 2015b or
+% higher.
+flag = license('test','neural_network_toolbox');
+aux = version('-release');
+
+if flag && str2double(aux(1:end-1))>=2015 && double(aux(end))>=double('a')
+    msgbox('Congratulations!!! You can run these files',...
+        'Welcome Box');
+else
+    warndlg('Maybe you need to update your MatLab version',...
+        'Warning');
+end
